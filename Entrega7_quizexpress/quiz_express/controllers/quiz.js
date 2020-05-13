c3c82aa491cfd1f0e070f795a001b617354fbebf
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2020-05-13 14:31:33 +0200

    Prática 2.1 - Quiz Express

---
+++ Entrega7_quizexpress/quiz_express/controllers/quiz.js
@@ -0,0 +1,10 @@
+const { models } = require("../models");
+
+exports.index = async (req, res, next) => {
+  try {
+    const quizzes = await models.Quiz.findAll();
+    res.render("quizzes/index", { quizzes });
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ Entrega7_quizexpress/quiz_express/routes/index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+
+const quizController = require("../controllers/quiz");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* GET /credits page. */
+router.get("/credits", function (req, res, next) {
+  res.render("credits");
+});
+
+/* GET /quizzes page. */
+router.get("/quizzes", quizController.index);
+
+module.exports = router;
